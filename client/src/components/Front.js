e36d3c617881800ef8111a6bc4dbf82aa5e8d406
14adit <aditya.cdu@gmail.com>
2024-02-15 07:22:58 +0000

    Add a customizable home page

--- client/src/components/Front.js
+++ client/src/components/Front.js
@@ -26,13 +26,13 @@ function Front() {
   return(
     <div className=''>
       <Navbar />
-    <div className="text-white pt-8 size-full bg-scroll bg-cover bg-" style={{backgroundImage:"url(/images/Trainplatform.jpg)"}}>
+    <div className="text-white pt-8 size-full bg-scroll bg-cover bg-" style={{backgroundImage:"url(/Trainplatform.jpg)"}}>
         <div className='pb-20'>
            <h1 className='text-center text-5xl font-semibold pt-5'>WELCOME TO <span className='text-orange-500 font-bold'>TicketBK</span></h1>
             <img className='object-cover float-right h-48 w-96 mr-36 rounded-lg mt-16' src="https://plus.unsplash.com/premium_photo-1679985958305-1677b87face0?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTN8fHRyYWlufGVufDB8fDB8fHww" />
            <p className='text-3xl mt-20 ml-20'>Book Tickets Here And Enjoy Your Travel 💕</p>
            <p className='text-l mt-3 ml-64 '>LET'S START TO EXPLORE  👇</p>
-           <Link to="/create-ticket" class="focus:outline-none text-black bg-yellow-400 hover:bg-yellow-500 focus:ring-4 focus:ring-yellow-300 font-bold rounded-full text-sm px-5 py-2.5 me-2 mb-2 dark:focus:ring-yellow-900 mt-4 ml-72 h-12 w-auto">Start</Link>
+           <Link to="/create-ticket" class="focus:outline-none text-black bg-yellow-400 hover:bg-yellow-500 focus:ring-4 focus:ring-yellow-300 font-bold rounded-full text-sm px-5 py-2.5 me-2 mb-2 dark:focus:ring-yellow-900 mt-8 ml-72 h-12 w-auto">Start</Link>
         </div>
            <div className='pt-8 pb-8'>
                <div className='container h-36 w-3/4 bg-zinc-900 rounded-full shadow-transparent flex flex-wrap border-1 border-white'>
