9e4feca8d193adeb40f421e53a4dbc460cbf15ff
14adit <aditya.cdu@gmail.com>
2024-02-19 17:47:06 +0000

    Add two nav menu

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -43,6 +43,8 @@ const Navbar = () => (
             <a href="/contact-us" class="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium ">Contact</a>
             <a href="/service" class="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">Service</a>
             <a href="#" class="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">About Us</a>
+            <a href="#" class="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">Document</a>
+            <a href="#" class="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">Routes</a>
           </div>
         </div>
       </div>     
